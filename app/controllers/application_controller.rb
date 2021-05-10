adacfc3bf9a6ea8d6e7bd2a7fe228dd294747fb6
Jason Zhou <jzhou35@buffalo.edu>
2021-05-10 15:25:21 -0400

    Backend set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def authenticate
+        token = request.headers["Authorization"].split.last
+        payload = JWT.decode(token, "mysecret", true, { algorithm: 'HS256'})
+        @current_user = User.find_by(id:  payload["user_id"])
+    rescue
+        render json: {errors: ["Not Authorized"]}, status: :unauthorized
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
-
+    before_action :authenticate, only: [:show]
     def login  
-        @user = User.find_by(email: params[:email])
-        if @user && @user.authenticate(params[:password])
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
             token = JWT.encode(
                 {user_id: user.id},
                 "mysecret",
                 "HS256"
             )
-            render json: {@user: UserSerializer.new(@user), token: token}
+            render json: {user: UserSerializer.new(user), token: token}
         else
             render json: {errors: ["Invalid email or password"], status: :unauthorized}
         end
     end 
 
     def signup
-        @user = User.create(user_params)
-        if @user.valid?
+        user = User.create(user_params)
+        if user.valid?
             token = JWT.encode(
                 {user_id: user.id},
                 "mysecret",
                 "HS256"
             )
-            render json: {@user: UserSerializer.new(@user), token: token}
+            render json: {user: UserSerializer.new(user), token: token}
         else
-            render json: {errors: @user.errors.full_messages, status: :unauthorized}
+            render json: {errors: user.errors.full_messages, status: :unauthorized}
         end
     end 
 
