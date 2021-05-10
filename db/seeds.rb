1d32aefc8557ebe43aaa2c9155fe786887fd8478
Erwin <efeng732@gmail.com>
2021-05-10 14:23:11 -0400

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 jason = User.create(name:"jason",password:"abc123",email:"jason@gmail.com")
 erwin = User.create(name:"erwin",password:"abc123",email:"erwin@gmail.com")
 daniel = User.create(name:"daniel",password:"abc123",email:"daniel@gmail.com")
+
+#is this going to be pushed?
